9a0568992b1061e4eba5f26952bed200bff14d61
krk324 <krk32488@gmail.com>
2014-06-09 17:25:07 -0400

    Added code to calculate depreceation.

--- vehicle_devaluate.rb
+++ vehicle_devaluate.rb
@@ -1,3 +1,17 @@
+puts "What is your first name: "
+first_name = gets.chomp
+puts "What is your last name: "
+last_name = gets.chomp
+puts "What is your car's name: "
+car = gets.chomp
+puts "What year did you purchase the car?: "
+year_purchased = gets.chomp.to_i
+puts "What is the price of the car?"
+price = gets.chomp.to_i
 
+year_difference = 2018 - year_purchased
 
-"___ purchased their first ___ in the year ___ they paided ___ dollars for it and it will be worth ___ dollars in the year 2018"
+new_price = price * (1+0.03)**-year_difference
+#new_price = [price - (price * 0.03) * (2018 - year_purchased),0].max
+
+puts "#{first_name} #{last_name} purchased their first #{car} in the year #{year_purchased} they paided #{price} dollars for it and it will be worth #{new_price} dollars in the year 2018"
